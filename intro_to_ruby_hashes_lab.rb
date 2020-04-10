f9db6171226fd64ba9bc07173d0c44bb714abacd
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-10 12:57:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,10 +23,22 @@ def monopoly_with_third_tier
       :pieces => 4, 
       :names => 
       {
-        :reading_railroad => {},
-        :pennsylvania_railroad => {},
-        :b_and_o_railroad => {},
-        :shortline => {}
+        :reading_railroad => 
+        {
+          :mortgage_values => '$100'
+        },
+        :pennsylvania_railroad => 
+        {
+          
+        },
+        :b_and_o_railroad => 
+        {
+          
+        },
+        :shortline => 
+        {
+          
+        }
       }, 
       :rent_in_dollars => 
       {
